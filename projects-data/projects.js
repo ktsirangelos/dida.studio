2742c1ed4c68ec751f1429b9c5203d209654ce97
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-07-01 13:47:00 +0200

    filter

---
+++ projects-data/projects.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    id: 1,
+    filter: "architecture",
+    title: "2.2.1 &nbsp; Almyros Resort",
+    type: "New Built",
+    year: 2023,
+    location: "Corfu, Greece",
+    direction: "USP",
+    description:
+      "Almyros Resort is a 5star luxury resort hotel in Northern Corfu, Greece, near the Natura area of the famous sand dunes. The proposal suggests a 75 room hotel with 13 separate room buildings, 3 common buildings, 2 common swimming pools, reaching nearly 7000sqm of built area.",
+    picUrl1: "projects-data/2.2.1/L0.webp",
+    picUrl2: "projects-data/2.2.1/P0.webp",
+    projectUrl: "projects-view/2.2.1.html",
+  },
+  {
+    id: 2,
+    filter: "architecture",
+    title: "2.2.2 &nbsp; EAS Paros",
+    type: "Masterplan",
+    year: 2022,
+    location: "Paros, Greece",
+    direction: "USP",
+    description:
+      "EAS Paros is a Union of Agricultural Cooperatives settled 650m from the coastal line of Paroikia, in Paros Aegean Island, founded in 1929, including a Winery, a Dairy Fram, an oil      Mill, a Barn, a Compost area with a Greenhouse and the Union’s Office space.",
+    picUrl1: "pictures/Lsmal.webp",
+    picUrl2: "pictures/Psmal.webp",
+    projectUrl: "projects-view/2.2.2.html",
+  },
+];
--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,29 +1,112 @@
-document.querySelectorAll(".accordion-button").forEach((button) => {
-  button.addEventListener("click", () => {
-    const accordionItem = button.parentElement;
-    const content = button.nextElementSibling;
+import { projects } from "../projects-data/projects.js";
 
-    // Close all other accordion items
-    document
-      .querySelectorAll(".accordion-item")
-      .forEach((otherAccordionItem) => {
-        if (otherAccordionItem !== accordionItem) {
-          otherAccordionItem
-            .querySelector(".accordion-button")
-            .classList.remove("active");
-          otherAccordionItem.querySelector(
-            ".accordion-content"
-          ).style.maxHeight = null;
-        }
-      });
+// Accordion DOM reference
+const accordion = document.querySelector(".accordion");
 
-    // Toggle the clicked accordion item
-    if (button.classList.contains("active")) {
-      button.classList.remove("active");
-      content.style.maxHeight = null;
+// Initial render of projects
+renderProjects(projects);
+
+// Filter items click event listener
+const filterItems = document.querySelectorAll(".filter-items a");
+
+filterItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const filterValue = event.target.id;
+    if (filterValue === "all") {
+      renderProjects(projects); // If 'all', render original projects array
     } else {
-      button.classList.add("active");
-      content.style.maxHeight = content.scrollHeight + "px";
+      filterProjects(filterValue); // Otherwise, filter and render
     }
   });
 });
+
+function filterProjects(filterValue) {
+  // Clone and sort projects based on filter
+  const sortedProjects = [...projects].sort((a, b) => {
+    return a.filter === filterValue ? -1 : 1;
+  });
+
+  // Render sorted projects
+  renderProjects(sortedProjects, filterValue); // pass filterValue here
+}
+
+function renderProjects(projectArray, filterValue) {
+  // Clear current accordion content
+  accordion.innerHTML = "";
+
+  // Render accordion with provided project array
+  projectArray.forEach((projectObject) => {
+    const disableButton =
+      filterValue &&
+      filterValue !== "all" &&
+      filterValue !== projectObject.filter;
+    const buttonClass = disableButton
+      ? "accordion-button disabled"
+      : "accordion-button";
+    accordion.innerHTML += `
+      <div class="accordion-item">
+        <button class="${buttonClass}">
+          <div class="accordion-button-title">
+            ${projectObject.title}
+          </div>
+          <div class="accordion-button-type">${projectObject.type}</div>
+          <div class="accordion-button-year">${projectObject.year}</div>
+        </button>
+        <div class="accordion-content">
+          <div class="accordion-content-pictures-1">
+            <a href="${projectObject.projectUrl}">
+              <img src="${projectObject.picUrl1}" alt="${projectObject.title}" />
+            </a>
+          </div>
+          <div class="accordion-content-pictures-2">
+            <a href="${projectObject.projectUrl}">
+              <img src="${projectObject.picUrl2}" alt="${projectObject.title}" />
+            </a>
+          </div>
+          <div class="accordion-content-text">
+            <p class="template">Location:</p>
+            <p>${projectObject.location}</p>
+            <p class="template">Creative Direction:</p>
+            <p>${projectObject.direction}</p>
+            <p class="template">Description:</p>
+            <p>
+              ${projectObject.description}
+            </p>
+            <a href="${projectObject.projectUrl}">explore</a>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  // Refresh accordion button event listeners
+  document.querySelectorAll(".accordion-button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const accordionItem = button.parentElement;
+      const content = button.nextElementSibling;
+
+      // Close all other accordion items
+      document
+        .querySelectorAll(".accordion-item")
+        .forEach((otherAccordionItem) => {
+          if (otherAccordionItem !== accordionItem) {
+            otherAccordionItem
+              .querySelector(".accordion-button")
+              .classList.remove("active");
+            otherAccordionItem.querySelector(
+              ".accordion-content"
+            ).style.maxHeight = null;
+          }
+        });
+
+      // Toggle the clicked accordion item
+      if (button.classList.contains("active")) {
+        button.classList.remove("active");
+        content.style.maxHeight = null;
+      } else {
+        button.classList.add("active");
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  });
+}
